621a57d0e98bf29833aea5d7b31c1d82735d2908
Carlos Aurélio <carlosync@gmail.com>
2016-08-14 19:42:31 -0400

    Configuração do ViewResolver.. Adicionando Thymeleaf no projeto.

--- src/main/java/com/brewer/controller/CervejasController.java
+++ src/main/java/com/brewer/controller/CervejasController.java
@@ -1,7 +1,13 @@
 package com.brewer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CervejasController {
+
+    @RequestMapping("/cerveja/novo")
+    public String novo(){
+        return "cerveja/CadastroCerveja";
+    }
 }
