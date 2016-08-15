04afffcbd1c1e38365ad82bffe20c19ad14ec1bb
Carlos Aurélio <carlosync@gmail.com>
2016-08-14 23:29:29 -0400

    Criação do Entidade Cerveja. Configuração e Ativação do Jrebel no projeto Modifação da anotação @RequestMapping para @GetMapping da versão 4.3 do Spring.

--- src/main/java/com/brewer/controller/CervejasController.java
+++ src/main/java/com/brewer/controller/CervejasController.java
@@ -1,12 +1,13 @@
 package com.brewer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CervejasController {
 
-    @RequestMapping("/cerveja/novo")
+    @GetMapping("cerveja/novo")
     public String novo(){
         return "cerveja/CadastroCerveja";
     }
