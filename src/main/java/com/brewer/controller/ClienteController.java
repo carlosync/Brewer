c36c0636530e99540a63edb7f862a1ec93747bcf
Carlos Aurélio <carlosync@gmail.com>
2016-08-21 18:21:39 -0400

    Criação da tela de cadastro de cleintes
    Implementação da tela de cadastro de clientes..

---
+++ src/main/java/com/brewer/controller/ClienteController.java
@@ -0,0 +1,14 @@
+package com.brewer.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ClienteController {
+
+    @RequestMapping("cliente/novo")
+    public String novo(){
+        return "cliente/CadastroCliente";
+    }
+}
