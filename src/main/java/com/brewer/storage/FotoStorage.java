e3b55fb11902887ef13a8a33fc8803d461296bb4
Carlos Aurélio <carlosync@gmail.com>
2017-02-22 19:50:25 -0400

    Várias modificações

---
+++ src/main/java/com/brewer/storage/FotoStorage.java
@@ -0,0 +1,7 @@
+package com.brewer.storage;
+
+/**
+ * Created by Carlos on 24/12/16.
+ */
+public interface FotoStorage {
+}
---
+++ src/main/java/com/brewer/storage/local/FotoStorageLocal.java
@@ -0,0 +1,41 @@
+package com.brewer.storage.local;
+
+import com.brewer.storage.FotoStorage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FotoStoageLocal implements FotoStorage{
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(FotoStoageLocal.class);
+
+    private Path local;
+    private Path localTemporario;
+
+    public FotoStoageLocal() {
+        this.local = FileSystems.getDefault().getPath(System.getenv("HOME"), ".brewerfotos");
+        criarPastas();
+    }
+
+    private void criarPastas() {
+        try {
+            Files.createDirectories(this.local);
+            this.localTemporario = FileSystems.getDefault().getPath(this.local.toString(), "temp");
+            Files.createDirectories(this.localTemporario);
+
+            if(LOGGER.isDebugEnabled()){
+                LOGGER.debug("Pasta criadas para salvar fotos");
+                LOGGER.debug("Pasta default " + this.local.toAbsolutePath());
+                LOGGER.debug("Pasta temporaria " + this.localTemporario.toAbsolutePath());
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Erro criando pasta para salvar as fotos", e);
+        }
+    }
+}
