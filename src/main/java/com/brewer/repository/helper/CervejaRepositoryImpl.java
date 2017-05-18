445b12c9c2c563ff7eeda88853f53adbcab32462
Carlos Aurélio <carlosync@gmail.com>
2017-05-18 10:28:03 -0400

    Implementação da ordenação na tela de pesquisa..

--- src/main/java/com/brewer/repository/helper/CervejaRepositoryImpl.java
+++ src/main/java/com/brewer/repository/helper/CervejaRepositoryImpl.java
@@ -6,11 +6,13 @@ import com.brewer.repository.filter.CervejaFilter;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
@@ -34,6 +36,13 @@ public class CervejaRepositoryImpl implements CervejasRepositoryQueries {
 
         criteria.setFirstResult(primeiroRegistro);
         criteria.setMaxResults(totalRegistroPorPagina);
+
+        Sort sort = pageable.getSort();
+        if (sort != null){
+            Sort.Order order = sort.iterator().next();
+            String field = order.getProperty();
+            criteria.addOrder(order.isAscending() ? Order.asc(field) : Order.desc(field));
+        }
 
         adicionarFiltro(filtro, criteria);
 
